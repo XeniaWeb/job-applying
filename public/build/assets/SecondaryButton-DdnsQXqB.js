4adcd7e7d1ead266571adb385e4d7ecf37b02011
XeniaWeb <xenia1898@gmail.com>
2024-06-17 12:02:46 +0200

    add build

---
+++ public/build/assets/SecondaryButton-DdnsQXqB.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./AppButton-DPaVbRTH.js";import{o as t,c as a,w as e,r as s}from"./app-sUdRIRSg.js";const m={__name:"SecondaryButton",setup(c){return(o,_)=>(t(),a(r,{color:"secondary"},{default:e(()=>[s(o.$slots,"default")]),_:3}))}};export{m as _};
