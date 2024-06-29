ae05b23a28e81738f4fb1da18ff807ca1098ade3
XeniaWeb <xenia1898@gmail.com>
2024-06-29 18:07:58 +0200

    add store Application Form, fix pagination error

---
+++ public/build/assets/SecondaryButton-CNXpzkmp.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./AppButton-C0Spxifx.js";import{o as t,b as a,w as e,k as s}from"./app-Dwfu4F7T.js";const m={__name:"SecondaryButton",setup(_){return(o,c)=>(t(),a(r,{color:"secondary"},{default:e(()=>[s(o.$slots,"default")]),_:3}))}};export{m as _};
